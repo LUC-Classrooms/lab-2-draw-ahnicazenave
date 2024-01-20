cafa150d09986a55f5775a3ebd2683a6f53d02ac
ahnicazenave <156936914+ahnicazenave@users.noreply.github.com>
2024-01-20 16:08:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ function setup() {
 function draw() {
   background(204, 226, 225);
   //floor
-  strokeWeight(60);
+  strokeWeight(80);
   stroke(246);
   line(0, 561, 800, 561);
   //arm left
   strokeWeight(2);
+  stroke(64);
   fill(64);
   rect(200, 250, 150, 10);
   //arm right
