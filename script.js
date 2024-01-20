17a2690c16bad625ba12b81fa96014ef111cf928
ahnicazenave <156936914+ahnicazenave@users.noreply.github.com>
2024-01-20 13:14:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function setup(){
+function setup() {
   createCanvas(480, 120);
 }
 
-function draw(){
-  background(200);
-  line(20, 50, 420, 110);// add your drawing code here ...
-  }
+function draw() {
+  background(204);
+  line(20, 50, 420, 110);
+}
