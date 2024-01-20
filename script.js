816960d0d7c1012c4669903613e3a00c149ca8a8
ahnicazenave <156936914+ahnicazenave@users.noreply.github.com>
2024-01-20 16:02:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ function setup() {
 function draw() {
   background(204, 226, 225);
   //floor
-  strokeWeight(20);
+  strokeWeight(40);
   stroke(250);
   line(0, 561, 800, 561);
   //arm left
-  strokeWeight(10);
+  strokeWeight(2);
   rect(200, 250, 150, 10);
   //arm right
   rect(450, 250, 150, 10);
   //body 2
-  fill(240);
+  fill(250);
   ellipse(400, 450, 225, 225);
   //body 1
-  fill(255, 0, 0);
+  fill(250);
   ellipse(400, 290, 200, 200);
   //head
-  fill(114);
+  fill(250);
   ellipse(400, 140, 175, 175);
 }
