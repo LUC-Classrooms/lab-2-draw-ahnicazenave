b58dc3a7681ea4a8ee8724be77571337c019de42
ahnicazenave <156936914+ahnicazenave@users.noreply.github.com>
2024-01-20 14:24:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function setup() {
 
 function draw() {
   background(240);
+  //body 2
+  ellipse(600, 300, 225, 225)
+  //body 1
+  ellipse(450, 151, 200, 200);
+  fill(255, 0, 0)
   //head
   ellipse(400, 150, 175, 175);
   fill(114);
-  //body 1
-  ellipse(450, 151, 200, 200);
-  fill(114)
 }
